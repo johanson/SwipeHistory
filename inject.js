1d02303ac6d8e7cf90b24716ea898a8f0a1d433a
Rasmus Johanson <rasmusjohanson@gmail.com>
2019-11-15 19:37:25 +0200

    Add options page

--- inject.js
+++ inject.js
@@ -1,38 +1,52 @@
-window.addEventListener("wheel",  direction);
+window.addEventListener("wheel", direction);
 
-let sensitivity = 80;
-let animation = true;
+var settings = new function () {
 
-function inject_css() {    
-    const sheet = new CSSStyleSheet();
-    sheet.replaceSync('body.swipeHistory { transition: opacity 0.2s; opacity: 0.2; overflow: hidden;' +
-                      'backface-visibility: hidden; pointer-events: none; }');
-    document.adoptedStyleSheets = [sheet];
-}
+    this.animation = true;
+    this.sensitivity = 50;
 
-if (animation) document.addEventListener('DOMContentLoaded', inject_css);
+    this.init = function () {
+        chrome.storage.sync.get({
+            animation: true,
+            sensitivity: 50
+        }, function (items) {
+            settings.set('animation', items.animation);
+            settings.set('sensitivity', items.sensitivity);
+            if (items.animation) settings.inject_css();
+        });
+    }
+    this.set = function (name, val) {
+        this[name] = val;
+    }
+    this.inject_css = function () {
+        window.addEventListener('DOMContentLoaded', (event) => {
+            const sheet = new CSSStyleSheet();
+            sheet.replaceSync('body.swipeHistory { transition: opacity 0.2s; opacity: 0.2; overflow: hidden;' +
+                'backface-visibility: hidden; pointer-events: none; }');
+            document.adoptedStyleSheets = [sheet];
+        });
+    }
+};
+
+settings.init();
 
 function deattach() {
-    window.removeEventListener("wheel",  direction);
+    window.removeEventListener("wheel", direction);
     // Remove wheel event listener but reattach it in case the user navigates back using browser history
     // Timeout is neccessary when a user tries to navigate forward but forward history is missing
-    setTimeout(function() {
-        window.addEventListener("wheel",  direction);
-        if (animation) document.body.classList.remove("swipeHistory");
+    setTimeout(function () {
+        window.addEventListener("wheel", direction);
+        document.body.classList.remove("swipeHistory");
     }, 1000);
 }
 
 function direction() {
-    let x = event.deltaX;
-    if (sensitivity == 0) {
-        console.log("Sensitivity must be bigger than 0")
-    }
-    if (x > sensitivity) {
-        if (animation && document.body) document.body.classList.add("swipeHistory");
+    if (event.deltaX > settings.sensitivity) {
+        document.body.classList.add("swipeHistory");
         deattach();
         window.history.forward();
-    } else if (x < -Math.abs(sensitivity)) {
-        if (animation && document.body) document.body.classList.add("swipeHistory");
+    } else if (event.deltaX < -Math.abs(settings.sensitivity)) {
+        document.body.classList.add("swipeHistory");
         deattach();
         window.history.back();
     }
---
+++ settings/settings.js
@@ -0,0 +1,25 @@
+function save_options() {
+    chrome.storage.sync.set({
+        animation: document.getElementById('animation').checked,
+        sensitivity: document.getElementById('sensitivity').value
+    }, function() {
+        document.getElementById("message").innerHTML = "Settings saved. Please refresh your tab to see the effect.";
+    })
+}
+
+function restore_options() {
+    chrome.storage.sync.get({
+        animation: true,
+        sensitivity: 50
+    }, function(items) {
+        document.getElementById('animation').checked = items.animation;
+        document.getElementById('sensitivity').value = items.sensitivity;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+
+document.getElementById('restore_defaults').addEventListener('click', function() {
+    document.getElementById('settings').reset();
+});
