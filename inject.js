22ad90109fc7842efb9973db1f01b0c677f2bb18
Rasmus Johanson <rasmusjohanson@gmail.com>
2019-11-16 22:25:40 +0200

    Add right click mousewheel navigation

--- inject.js
+++ inject.js
@@ -1,19 +1,21 @@
-window.addEventListener("wheel", direction);
+var conf = new function () {
 
-var settings = new function () {
-
-    this.animation = true;
-    this.sensitivity = 50;
+    this.right_click_down = false;
+    this.deattach_clear = true;
 
     this.init = function () {
         chrome.storage.sync.get({
             animation: true,
-            sensitivity: 50
+            sensitivity: 50,
+            rbtn_scroll: false
         }, function (items) {
-            settings.set('animation', items.animation);
-            settings.set('sensitivity', items.sensitivity);
-            if (items.animation) settings.inject_css();
+            for (key in items) {
+                conf.set(key, items[key]);
+            }
+            if (items.animation) conf.inject_css();
+            if (items.rbtn_scroll) window.addEventListener("mousedown", poll_mouse);
         });
+        window.addEventListener("wheel", poll_wheel);
     }
     this.set = function (name, val) {
         this[name] = val;
@@ -28,26 +30,41 @@ var settings = new function () {
     }
 };
 
-settings.init();
-
 function deattach() {
-    window.removeEventListener("wheel", direction);
     // Remove wheel event listener but reattach it in case the user navigates back using browser history
     // Timeout is neccessary when a user tries to navigate forward but forward history is missing
-    setTimeout(function () {
-        window.addEventListener("wheel", direction);
-        document.body.classList.remove("swipeHistory");
-    }, 1000);
+    if (conf.deattach_clear) {
+        window.removeEventListener("wheel", poll_wheel);
+        if (conf.rbtn_scroll) {
+            window.removeEventListener("mousedown", poll_mouse);
+            document.oncontextmenu = function () { return false; }
+        }
+        document.body.classList.add("swipeHistory");
+        setTimeout(function () {
+            window.addEventListener("wheel", poll_wheel);
+            document.body.classList.remove("swipeHistory");
+            conf.deattach_clear = true;
+            if (conf.rbtn_scroll) {
+                window.removeEventListener("mousedown", poll_mouse);
+                document.oncontextmenu = function () { return true; }
+            }
+        }, 1250);
+    }
+    conf.deattach_clear = false;
 }
 
-function direction() {
-    if (event.deltaX > settings.sensitivity) {
-        document.body.classList.add("swipeHistory");
+function poll_wheel() {
+    if (event.deltaX > conf.sensitivity || conf.right_click_down && event.deltaY < 0) {
         deattach();
         window.history.forward();
-    } else if (event.deltaX < -Math.abs(settings.sensitivity)) {
-        document.body.classList.add("swipeHistory");
+    } else if (event.deltaX < -Math.abs(conf.sensitivity) || conf.right_click_down && event.deltaY > 0) {
         deattach();
         window.history.back();
     }
 }
+
+function poll_mouse() {
+    conf.right_click_down = (event.which == 3) ? true : false;
+}
+
+conf.init();
--- settings/settings.js
+++ settings/settings.js
@@ -1,8 +1,9 @@
 function save_options() {
     chrome.storage.sync.set({
         animation: document.getElementById('animation').checked,
-        sensitivity: document.getElementById('sensitivity').value
-    }, function() {
+        sensitivity: document.getElementById('sensitivity').value.animation,
+        rbtn_scroll: document.getElementById('rbtn_scroll').checked,
+    }, function () {
         document.getElementById("message").innerHTML = "Settings saved. Please refresh your tab to see the effect.";
     })
 }
@@ -10,16 +11,18 @@ function save_options() {
 function restore_options() {
     chrome.storage.sync.get({
         animation: true,
-        sensitivity: 50
-    }, function(items) {
+        sensitivity: 50,
+        rbtn_scroll: false
+    }, function (items) {
         document.getElementById('animation').checked = items.animation;
         document.getElementById('sensitivity').value = items.sensitivity;
-  });
+        document.getElementById('rbtn_scroll').checked = items.rbtn_scroll;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
 
-document.getElementById('restore_defaults').addEventListener('click', function() {
+document.getElementById('restore_defaults').addEventListener('click', function () {
     document.getElementById('settings').reset();
 });
