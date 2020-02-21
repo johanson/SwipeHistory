56b6c0805be3adc164e658873580a2f967e66233
Rasmus Johanson <rasmusjohanson@gmail.com>
2020-02-21 04:09:51 +0200

    remove custom new tab

--- settings/settings.js
+++ settings/settings.js
@@ -2,7 +2,6 @@ function save_options() {
     chrome.storage.sync.set({
         animation: document.getElementById('animation').checked,
         sensitivity: document.getElementById('sensitivity').value,
-        rbtn_scroll: document.getElementById('rbtn_scroll').checked,
     }, function () {
         document.getElementById("message").innerHTML = "Settings saved. Please refresh your tab to see the effect.";
     })
@@ -12,12 +11,12 @@ function restore_options() {
     chrome.storage.sync.get({
         animation: true,
         sensitivity: 50,
-        rbtn_scroll: false
+        custom_new_tab: false,
+        custom_new_tab_url: 'blank'
     }, function (items) {
         document.getElementById('animation').checked = items.animation;
         document.getElementById('sensitivity').value = items.sensitivity;
-        document.getElementById('rbtn_scroll').checked = items.rbtn_scroll;
-    });
+        document.getElementById('custom_new_tab').checked = items.custom_new_tab;
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
